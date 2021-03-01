210ff3094f6e9987dbf45f89569aab5687d73b8e
Doug Robinson <33792770+b-douglas@users.noreply.github.com>
2021-03-01 14:26:56 -0500

    Mon-Mar01-1426 dougz updates to repository

--- kata/__tests__/catchcarmileage.test.js
+++ kata/__tests__/catchcarmileage.test.js
@@ -18,24 +18,27 @@ let Test = new TestClass()
 
 describe("Basic inputs", function () {
   test("should work, dangit!", function () {
-    //Test.assertEquals(isInteresting(3, [1337, 256]), 0)
-    //Test.assertEquals(isInteresting(111, [1337, 256]), 0)
-    // Test.assertEquals(isInteresting(1336, [1337, 256]), 1)
-    // Test.assertEquals(isInteresting(1337, [1337, 256]), 2)
+    Test.assertEquals(isInteresting(3, [1337, 256]), 0)
+    Test.assertEquals(isInteresting(111, [1337, 256]), 2)
+    Test.assertEquals(isInteresting(1336, [1337, 256]), 1)
+    Test.assertEquals(isInteresting(1337, [1337, 256]), 2)
+    Test.assertEquals(isInteresting(7734, [7734, 256, 7736, 707, 543]), 2)
+    Test.assertEquals(isInteresting(7734, [7736, 256, 256, 7735, 7734]), 2)
+    Test.assertEquals(isInteresting(7734, [7736, 256, 256, 7735, 707]), 1)
+    Test.assertEquals(isInteresting(1000, []), 2)
+    Test.assertEquals(isInteresting(9000, []), 2)
+    Test.assertEquals(isInteresting(9999, []), 2)
+    Test.assertEquals(isInteresting(9998, []), 1)
+    Test.assertEquals(isInteresting(1999, []), 1)
+    Test.assertEquals(isInteresting(7890, []), 2)
+    Test.assertEquals(isInteresting(7889, []), 1)
+    Test.assertEquals(isInteresting(8901, []), 0)
+    Test.assertEquals(isInteresting(4320, []), 1)
+    Test.assertEquals(isInteresting(985, []), 1)
+    Test.assertEquals(isInteresting(1098, []), 0)
+
     // Test.assertEquals(isInteresting(11208, [1337, 256]), 0)
     // Test.assertEquals(isInteresting(11209, [1337, 256]), 1)
-    //Test.assertEquals(isInteresting(11211, [1337, 256, 58008, 707, 7734]), 2)
-
-    // Test.assertEquals(isInteresting(7734, [7734, 256, 7736, 707, 543]), 2)
-
-    // Test.assertEquals(isInteresting(7734, [7736, 256, 256, 7735, 7734]), 2)
-
-    // Test.assertEquals(isInteresting(7734, [7736, 256, 256, 7735, 707]), 1)
-
-    Test.assertEquals(isInteresting(1000, []), 2)
-
-    Test.assertEquals(isInteresting(9998, []), 1)
-
-    Test.assertEquals(isInteresting(9997, []), 0)
+    // Test.assertEquals(isInteresting(11211, [1337, 256, 58008, 707, 7734]), 2)
   })
 })
--- kata/catchcarmileage.js
+++ kata/catchcarmileage.js
@@ -1,25 +1,85 @@
+const log = []
+
 export default class CatchCarMileage {
   static trace(str) {
     const isTrace = true
+    if (isTrace) {
+      log.push(str)
+    }
+  }
+
+  static traceout() {
+    const isTrace = true
 
     if (isTrace) {
-      console.log(str)
+      console.log(log.join("\n"))
     }
+    log.length = 0
+  }
+
+  static allSameDigit(firstDigit, numLen) {
+    let num = 0
+    for (let i = 0; i <= numLen; i++) {
+      num = num + firstDigit * 10 ** i
+    }
+    return num
+  }
+
+  static seqDec(firstDigit, numLen) {
+    let num = 0
+    let digit = firstDigit
+    let part = 0
+    for (let i = numLen; digit >= 0 && digit <= 9 && i >= 0; i--) {
+      part = digit * 10 ** i
+      num = num + part
+      if (digit === 1 && i > 0) {
+        num = -1
+        break
+      } else if (digit === 0) {
+        digit = 9
+      } else {
+        digit--
+      }
+    }
+    CatchCarMileage.trace(`seqDec - ${firstDigit} ${numLen} ${num}`)
+    return num
+  }
+
+  static seqInc(firstDigit, numLen) {
+    let num = 0
+    let digit = firstDigit
+    let part = 0
+    for (let i = numLen; digit >= 0 && digit <= 9 && i >= 0; i--) {
+      part = digit * 10 ** i
+      num = num + part
+      if (digit === 0 && i > 0) {
+        num = -1
+        break
+      }
+      digit++
+      if (digit === 10) {
+        digit = 0
+      }
+    }
+    CatchCarMileage.trace(`seqInc - ${firstDigit} ${numLen} ${num}`)
+    return num
   }
 
   static isInteresting(number, awesomePhrases) {
     // Go to town!
 
-    CatchCarMileage.trace(`>CatchCarmMileage::isInteresting`)
+    CatchCarMileage.trace(`>CatchCarmMileage::isInteresting ${number} [${awesomePhrases.toString()}]`)
 
     const no = 0
     const almost = 1
     const yes = 2
+    const numsToCheck = awesomePhrases
 
     let interesting = no
 
     if (number <= 99 || number >= 1000000000) {
       CatchCarMileage.trace(`<CatchCarmMileage::isInteresting ${interesting}`)
+      CatchCarMileage.traceout()
       return no
     }
 
@@ -28,46 +88,50 @@ export default class CatchCarMileage {
       down: [],
     }
 
-    let i = 0
-    for (i = 10; i <= number; i *= 10) {
-      let digit = ((number % (i * 10)) / i).toFixed(0)
-      CatchCarMileage.trace(`${i} ${digit} ${number}`)
-
-      if (i === 10) {
-        if (digit == 0) {
-          sequentials.up = []
-          sequentials.down = []
-        }
-        sequentials.up = digit * 10 + (digit + 1)
-        sequentials.down
-      }
+    let numLen = 0
+    let firstDigit = -1
+    for (let i = 10; i <= number; i *= 10) {
+      firstDigit = Math.floor((number % (i * 10)) / i)
+      numLen++
     }
+    const tenToThe = 10 ** numLen
 
-    //Check for Zeros
+    CatchCarMileage.trace(`AL - ${number} ${firstDigit} ${numLen} ${tenToThe}`)
+
+    // Add for Zeros
+    numsToCheck.push(firstDigit * tenToThe)
+    numsToCheck.push((firstDigit + 1) * tenToThe)
+
+    //Add for all same digit
+    //allSameDigit
+    numsToCheck.push(CatchCarMileage.allSameDigit(firstDigit, numLen))
+
+    //Add for all The digits are sequential, incementing†: 1234
+    numsToCheck.push(CatchCarMileage.seqInc(firstDigit, numLen))
+
+    //    The digits are sequential, decrementing‡: 4321
+    numsToCheck.push(CatchCarMileage.seqDec(firstDigit, numLen))
+
+    //    The digits are a palindrome: 1221 or 73837
+    // numsToCheck.push(CatchCarMileage.palindrome(firstDigit, numLen))
+
+    CatchCarMileage.trace(`numsToCheck[${numsToCheck.toString()}]`)
+
     if (interesting != yes) {
-      //CatchCarMileage.trace(`${(number + 1) % (i / 10)}\n${number % (i / 10)}\n${(number + 2) % (i / 10)}`)
-
-      if ((number + 1) % (i / 10) === 0 || (number + 2) % (i / 10) === 0) {
-        interesting = almost
-      } else if (number % (i / 10) === 0) {
-        interesting = yes
-      }
-    }
-
-    if (interesting != yes) {
-      for (const n of awesomePhrases) {
+      for (const n of numsToCheck) {
         if (n === number) {
           interesting = yes
-          CatchCarMileage.trace(`awesomePhrases ${n} === ${number} ${interesting}`)
+          CatchCarMileage.trace(`numsToCheck ${n} === ${number} ${interesting}`)
           break
         } else if (number >= n - 2 && number < n) {
           interesting = almost
-          CatchCarMileage.trace(`awesomePhrases ${number} is almost ${n}  ${number >= n - 2} ${number < n}`)
+          CatchCarMileage.trace(`numsToCheck ${number} is almost ${n}  ${number >= n - 2} ${number < n}`)
         }
       }
     }
 
     CatchCarMileage.trace(`<CatchCarmMileage::isInteresting ${interesting}`)
+    CatchCarMileage.traceout()
     return interesting
   }
 }
